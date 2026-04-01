3f00452bf1914079c938814a1df064f9f0218a63
Renan Cavalcanti <renan_cavalcanti@hotmail.com>
2026-04-01 14:29:35 -0400

    chore: lab answer

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/auth", authRoutes);
+app.use("/tasks", taskRoutes);
 
 module.exports = app;
